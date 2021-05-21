9e889e0b1082eaa47e4c974cedfb1cf10290f57b
Salome Ranson <salomeranson@gmail.com>
2021-05-21 16:14:40 -0400

    added all posts to home page logged out with username and formatted date

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,32 @@
 const router = require('express').Router();
 const { Post, User } = require('../models');
-// const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth');
 
 
-router.get('/', (req, res) => {
-    res.render('home');
-});
-
+router.get('/', async (req, res) => {
+    try {
+      // Get all posts and JOIN with user data
+      const postData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['username'],
+          },
+        ],
+      });
+  
+      // Serialize data so the template can read it
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('home-loggedOut', { 
+        posts, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 router.get('/login', (req, res) => {
     res.render('login');
